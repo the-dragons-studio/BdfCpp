41b4a73b09a8be9782a0dbf0f91d8bf1e3640aec
the-dragons-studio <62834738+the-dragons-studio@users.noreply.github.com>
2023-12-26 10:39:03 +1000

    Constructor changes
    Added new std::filesystem::path constructor, marked all constructors explicit, and added Doxygen documentation.

--- include/BdfReaderHuman.hpp
+++ include/BdfReaderHuman.hpp
@@ -3,14 +3,36 @@
 #define BDFREADERHUMAN_HPP_
 
 #include <string>
+#include <filesystem>
 
 namespace Bdf
 {
+	/**
+	 * Class for reading BDF files that are human-readable.
+	 */
 	class BdfReaderHuman : public BdfReader
 	{
 	public:
-		BdfReaderHuman(std::string data);
-		BdfReaderHuman(std::wstring data);
+		/**
+		 * Parses data as a human-readable BDF file.
+		 * @param data narrow-encoded text representing a human-readable BDF file.
+		 * @throw BdfError if data could not be parsed.
+		 */
+		explicit BdfReaderHuman(std::string data);
+		
+		/**
+		 * Parses data as a human-readable BDF file.
+		 * @param data wide-encoded text representing a human-readable BDF file.
+		 * @throw BdfError if data could not be parsed.
+		 */
+		explicit BdfReaderHuman(std::wstring data);
+		
+		/**
+		 * Opens the file located at filename as a BDF file.
+		 * @param filename the file to read as a BDF file.
+		 * @throw BdfError if data could not be parsed.
+		 */
+		explicit BdfReaderHuman(std::filesystem::path filename);
 	};
 }
 
