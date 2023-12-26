7fda8602a03814df9cbae3059df8980bd8cfd6fd
the-dragons-studio <62834738+the-dragons-studio@users.noreply.github.com>
2023-12-26 10:33:44 +1000

    Add version.hpp
    Add version.hpp for portably getting BdfCpp's version and build date

---
+++ include/version.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <string>
+
+namespace Bdf {
+	/**
+	 * The header version.
+	 * @todo Convert this to use cmake instead.
+	 */
+	const std::string VERSION = "2.0.0-alpha";
+	
+	/**
+	 * The header codename.
+	 */
+	const std::string CODENAME = "AMBERLEY";
+	
+	/**
+	 * Get the build date of the library currently in use with BdfCpp.
+	 * If using a static library, returns the build date of libbdf.a. If using a shared library, returns the build date of libbdf.dll or libbdf.so.
+	 * @return the build date in ISO 8601 UTC format.
+	 */
+	const std::string getLibraryBuildDate();
+	
+	/**
+	 * Get the version of the library currently in use with bdf.
+	 * If using a static library, returns the version of libbdf.a. If using a shared library, returns the version of libbdf.dll or libbdf.so.
+	 * @return a string containing the library version.
+	 */
+	const std::string getLibraryVersion();
+}	
