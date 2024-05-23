1e5ffd9cf177cb6250dd4c48939d6d9576018459
the-dragons-studio <62834738+the-dragons-studio@users.noreply.github.com>
2024-05-23 11:11:00 +1000

    Enforce whitespace and newline requirement
    Only containing whitespace and/or newlines was already required by the Doxygen documentation, but this now enforces it.

--- src/BdfIndent.cpp
+++ src/BdfIndent.cpp
@@ -1,9 +1,15 @@
 
 #include "../include/BdfIndent.hpp"
 
+#include <stdexcept>
+
 using namespace Bdf;
 
-BdfIndent::BdfIndent(std::string pIndent, std::string pBreaker) {
-  indent = pIndent;
-  breaker = pBreaker;
+BdfIndent::BdfIndent(std::string pIndent, std::string pBreaker): indent(pIndent), breaker(pBreaker) {
+    // Check that all characters in the strings don't have illegal characters
+    if (this->indent.find_first_not_of("\t\n ") != string::npos) {
+        throw std::invalid_argument(std::string("bdf::BdfIndent: Indenter \"") + this->indenter + "\" contains non-whitespace or newline characters.");
+    } else if (this->breaker.find_first_not_of("\t\n ") != string::npos) {
+        throw std::invalid_argument(std::string("bdf::BdfIndent: Breaker \"") + this->breaker + "\" contains non-whitespace or newline characters.");
+    }
 }
