6cde38ab9ab4475bf8d184321f26e126028ea295
the-dragons-studio <62834738+the-dragons-studio@users.noreply.github.com>
2024-05-23 17:05:18 +1000

    indenter -> indent

--- src/BdfIndent.cpp
+++ src/BdfIndent.cpp
@@ -8,7 +8,7 @@ using namespace Bdf;
 BdfIndent::BdfIndent(std::string pIndent, std::string pBreaker): indent(pIndent), breaker(pBreaker) {
     // Check that all characters in the strings don't have illegal characters
     if (this->indent.find_first_not_of("\t\n ") != std::string::npos) {
-        throw std::invalid_argument(std::string("Bdf::BdfIndent: Indenter \"") + this->indenter + "\" contains non-whitespace or newline characters.");
+        throw std::invalid_argument(std::string("Bdf::BdfIndent: Indenter \"") + this->indent + "\" contains non-whitespace or newline characters.");
     } else if (this->breaker.find_first_not_of("\t\n ") != std::string::npos) {
         throw std::invalid_argument(std::string("Bdf::BdfIndent: Breaker \"") + this->breaker + "\" contains non-whitespace or newline characters.");
     }
