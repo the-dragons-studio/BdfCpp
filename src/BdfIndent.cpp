a0d2d6cb46f2f56c0a96114af0c0702dc5cdfccf
the-dragons-studio <62834738+the-dragons-studio@users.noreply.github.com>
2024-05-23 11:14:08 +1000

    Fix

--- src/BdfIndent.cpp
+++ src/BdfIndent.cpp
@@ -7,9 +7,9 @@ using namespace Bdf;
 
 BdfIndent::BdfIndent(std::string pIndent, std::string pBreaker): indent(pIndent), breaker(pBreaker) {
     // Check that all characters in the strings don't have illegal characters
-    if (this->indent.find_first_not_of("\t\n ") != string::npos) {
+    if (this->indent.find_first_not_of("\t\n ") != std::string::npos) {
         throw std::invalid_argument(std::string("bdf::BdfIndent: Indenter \"") + this->indenter + "\" contains non-whitespace or newline characters.");
-    } else if (this->breaker.find_first_not_of("\t\n ") != string::npos) {
+    } else if (this->breaker.find_first_not_of("\t\n ") != std::string::npos) {
         throw std::invalid_argument(std::string("bdf::BdfIndent: Breaker \"") + this->breaker + "\" contains non-whitespace or newline characters.");
     }
 }
