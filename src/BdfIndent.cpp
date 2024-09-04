1cce8c8493b2c926bc00aa0ccda601875c98c725
the-dragons-studio <62834738+the-dragons-studio@users.noreply.github.com>
2024-09-05 09:59:50 +1000

    BdfIndent: Remove whitespace enforcer
    There may well be legitimate uses for non-whitespace characters in a BdfIndent (mostly to do with 2.0's comment classes), and their presence may break REL1_4's compatibility gurantee with 1.4.0 and 1.4.1.

--- src/BdfIndent.cpp
+++ src/BdfIndent.cpp
@@ -5,11 +5,4 @@
 
 using namespace Bdf;
 
-BdfIndent::BdfIndent(std::string pIndent, std::string pBreaker): indent(pIndent), breaker(pBreaker) {
-    // Check that all characters in the strings don't have illegal characters
-    if (this->indent.find_first_not_of("\t\n ") != std::string::npos) {
-        throw std::invalid_argument(std::string("Bdf::BdfIndent: Indenter \"") + this->indent + "\" contains non-whitespace or newline characters.");
-    } else if (this->breaker.find_first_not_of("\t\n ") != std::string::npos) {
-        throw std::invalid_argument(std::string("Bdf::BdfIndent: Breaker \"") + this->breaker + "\" contains non-whitespace or newline characters.");
-    }
-}
+BdfIndent::BdfIndent(std::string pIndent, std::string pBreaker): indent(pIndent), breaker(pBreaker) {}
