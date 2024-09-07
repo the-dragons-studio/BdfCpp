1b1f7d267281eca05d7160d06b670f7221381f6f
the-dragons-studio <62834738+the-dragons-studio@users.noreply.github.com>
2024-09-08 01:00:43 +1000

    Add BdfIndent::calcIndent
    BdfIndent::calcIndent is based on BdfObject::calcIndent in 1.4 or less, but moved to BdfIndent to benefit other classes.

--- include/BdfIndent.hpp
+++ include/BdfIndent.hpp
@@ -6,14 +6,32 @@
 
 namespace Bdf
 {
+	/**
+	 * Class used to configure indenting and line breaking of serialising human-readable BDF files.
+  	 * @since 1.0
+         * @newable
+	 */
 	class BdfIndent
 	{
 	public:
 	
 		std::string indent;
 		std::string breaker;
-	
+		
+		/**
+		 * Creates an indenter which can be passed to various serialising methods to customise
+                 * how they will be indented.
+		 * @param indent the string that will be used to indent (for example, in a BdfNamedList).
+		 * @param breaker the string that will be used to line break at the end of the line.
+		 */
 		BdfIndent(std::string indent, std::string breaker);
+
+		/**
+                 * Returns the indent string repeated n number of times.
+                 * @param n the number of times to repeat the indent string.
+                 * @since 2.0.0
+                 */
+                std::string calcIndent(int n);
 	};
 }
 
