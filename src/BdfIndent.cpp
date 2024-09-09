ffb4c07e4cb4e5018edfcd72bb8c47da669d3acf
the-dragons-studio <62834738+the-dragons-studio@users.noreply.github.com>
2024-09-09 13:37:44 +1000

    Remove unnecessary "" assignment
    Because std::string's default constructor initialises to an empty string anyway, assigning from an empty const char is unnecessary.

--- src/BdfIndent.cpp
+++ src/BdfIndent.cpp
@@ -10,7 +10,7 @@ BdfIndent::BdfIndent(std::string pIndent, std::string pBreaker): indent(pIndent)
 // Moved from BdfObject::calcIndent() to be more useful to other classes that need it.
 // @todo: Try to make constexpr?
 std::string BdfIndent::calcIndent(int it) {
-	std::string t = "";
+	std::string t;
 
 	for(int i=0; i<=it;i ++) {
 		t += this->indent;
