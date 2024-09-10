8a147fdf365c81993b7501c3fe8d1200306ae0f7
the-dragons-studio <62834738+the-dragons-studio@users.noreply.github.com>
2024-09-10 15:55:05 +1000

    Make release: v1.4.4 (1.4.4-alpha -> 1.4.4)

--- include/version.hpp
+++ include/version.hpp
@@ -7,7 +7,7 @@ namespace Bdf {
 	 * The header version.
 	 * @todo Convert this to use cmake instead.
 	 */
-	const std::string VERSION = "1.4.4-alpha";
+	const std::string VERSION = "1.4.4";
 	
 	/**
 	 * The header codename.
