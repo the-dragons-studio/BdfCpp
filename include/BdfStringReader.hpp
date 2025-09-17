72f23acc1390eb8c211f566b9a4d8d4fd0e7f88c
the-dragons-studio <62834738+the-dragons-studio@users.noreply.github.com>
2025-09-17 16:24:07 +1000

    BdfStringReader: Pass isNext(check) by const reference instead of by value

--- include/BdfStringReader.hpp
+++ include/BdfStringReader.hpp
@@ -29,7 +29,7 @@ namespace Bdf
 		 */
 		bool ignoreBlanks();
 		std::string getQuotedString();
-		bool isNext(std::wstring check);
+		bool isNext(const std::wstring &check);
 		bool inRange();
 	};
 }
