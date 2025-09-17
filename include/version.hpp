765e1a68ced7f5884edf2a7a2bffb80338ff196b
the-dragons-studio <62834738+the-dragons-studio@users.noreply.github.com>
2025-09-17 14:14:18 +1000

    Start development on v1.4.6 (1.4.5 -> 1.4.6-alpha)

--- include/version.hpp
+++ include/version.hpp
@@ -7,7 +7,7 @@ namespace Bdf {
 	 * The header version.
 	 * @todo Convert this to use cmake instead.
 	 */
-	const std::string VERSION = "1.4.5";
+	const std::string VERSION = "1.4.6-alpha";
 	
 	/**
 	 * The header codename.
