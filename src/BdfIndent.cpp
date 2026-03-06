e64884ebc8daf5903643404ec71b08114a9a9f62
the-dragons-studio <62834738+the-dragons-studio@users.noreply.github.com>
2026-03-06 17:47:53 +1000

    BdfIndent: constexpr overhaul and add minimum indent level capability

--- src/BdfIndent.cpp
+++ src/BdfIndent.cpp
@@ -1,20 +1,8 @@
 
 #include "../include/BdfIndent.hpp"
 
-#include <utility>
-
 using namespace Bdf;
 
-BdfIndent::BdfIndent(std::string pIndent, std::string pBreaker): indent(std::move(pIndent)), breaker(std::move(pBreaker)) {}
+// BdfIndent::BdfIndent defined at BdfIndent.hpp inline due to needing constexpr.
 
-// Moved from BdfObject::calcIndent() to be more useful to other classes that need it.
-// @todo: Try to make constexpr?
-std::string BdfIndent::calcIndent(int it) const {
-	std::string t;
-
-	for(int i=0; i<=it;i ++) {
-		t += this->indent;
-	}
-
-	return t;
-}
+// BdfIndent::calcIndent defined at BdfIndent.hpp inline due to needing constexpr.
