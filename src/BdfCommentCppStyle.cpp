0cd2308c6ec8811ec95854c821fbe953a468a262
the-dragons-studio <62834738+the-dragons-studio@users.noreply.github.com>
2026-09-16 09:52:30 +1000

    BdfCommentCppStyle: Rename flattenSingleLineComments to flatMode, add collection of getters/setters

--- src/BdfCommentCppStyle.cpp
+++ src/BdfCommentCppStyle.cpp
@@ -7,33 +7,48 @@ namespace Bdf {
 	}
 	
 	// Delegate to vector constructor
-	BdfCommentCppStyle::BdfCommentCppStyle(std::string commentText, Bdf::BdfIndent indenter, bool flattenSingleLineComments):
-		BdfCommentCppStyle(std::vector<std::string>({commentText}), std::move(indenter), flattenSingleLineComments) {
+	BdfCommentCppStyle::BdfCommentCppStyle(std::string commentText, Bdf::BdfIndent indenter, bool flatMode):
+		BdfCommentCppStyle(std::vector<std::string>({commentText}), std::move(indenter), flatMode) {
 	}
 	
 	// Stores the given vector
-	BdfCommentCppStyle::BdfCommentCppStyle(std::vector<std::string> commentLines, Bdf::BdfIndent indenter, bool flattenSingleLineComments): 
+	BdfCommentCppStyle::BdfCommentCppStyle(std::vector<std::string> commentLines, Bdf::BdfIndent indenter, bool flatMode): 
 		commentLines(std::move(commentLines)),
 		indenter(std::move(indenter)),
-		flattenSingleLineComments(flattenSingleLineComments) {
+		flatMode(flatMode) {
 	}
 	
-	void BdfCommentCppStyle::setCommentLines(const std::vector<std::string> &commentLines) {
-		this->commentLines = commentLines;
+	std::vector<std::string> BdfCommentCppStyle::getCommentLines() const {
+		return this->commentLines;
 	}
 	
-	void BdfCommentCppStyle::setCommentLines(std::vector<std::string> &&commentLines) {
+	Bdf::BdfIndent BdfCommentCppStyle::getIndenter() const {
+		return this->indenter;
+	}
+		
+	bool BdfCommentCppStyle::getFlatMode() const noexcept {
+		return this-flatMode;
+	}
+	
+	void BdfCommentCppStyle::setCommentLines(std::vector<std::string> commentLines) {
 		this->commentLines = std::move(commentLines);
 	}
 	
+	void BdfCommentCppStyle::setIndenter(Bdf::BdfIndent indenter) {
+		this->indenter = std::move(indenter);
+	}
+	
+	void BdfCommentCppStyle::setFlatMode(bool flatMode) noexcept {
+		this->flatMode = flatMode;
+	}
 	
 	BdfCommentCppStyle::~BdfCommentCppStyle() noexcept = default;
 	
 	void BdfCommentCppStyle::serializeHumanReadable(std::ostream &stream, const BdfIndent &foreignIndent, int upto) const {
 		// Iterate through each stored comment line
 		for (std::string c : this->commentLines) {
-			stream << foreignIndent.breaker << this->indenter.breaker << this->indenter.calcIndent(upto);
 			stream << std::string("// ") << c;
+			stream << foreignIndent.breaker << this->indenter.breaker << this->indenter.calcIndent(upto);
 		}
 	}
 }
