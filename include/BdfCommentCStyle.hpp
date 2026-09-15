19d1a52a8a340b3dcb8ba1f77d33dab89403fedb
the-dragons-studio <62834738+the-dragons-studio@users.noreply.github.com>
2026-09-16 09:12:47 +1000

    BdfCommentCStyle: Rename flattenSingleLineComments to flatMode, add full getter/setter set

--- include/BdfCommentCStyle.hpp
+++ include/BdfCommentCStyle.hpp
@@ -7,30 +7,90 @@
 #define BDFCOMMENTCSTYLE_HPP_
 namespace Bdf {
 	/**
-	 * Class that represents a single line BDF comment.
+	 * Class that represents a C-style BDF comment.
+	 * These can be added to BdfObject, BdfNamedList or BdfList objects and will
+	 * be serialised if human-readable serialisation is called.
+	 * They will not be serialised to binary BDF output, nor will they be unserialised
+	 * if the resulting human-readable BDF data is unserialised again.
+	 * Therefore, a round-trip BdfObject serialisation will not result in an equal unserialised
+	 * object again.
+	 * Use BdfObject::coerceCommentCStyle() or similar to start working with objects of this class.
 	 * @since 1.5.0
 	 */
 	class BdfCommentCStyle {
 		std::vector<std::string> commentLines;
 		Bdf::BdfIndent indenter;
-		bool flattenSingleLineComments;
+		bool flatMode;
 		
 		public:
 		/**
 		 * Creates a BdfCommentCStyle comment which is empty.
-		 * The comment line delimiter will still be created.
+		 * The comment line delimiter will still be serialised.
+		 * @param indenter The new object's indenter.
+		 * @param flatMode The new object's flatMode.
+		 * @since 1.5.0
+		 * @internal
 		 */
-		BdfCommentCStyle();
+		BdfCommentCStyle(Bdf::BdfIndent indenter = {"", "\n"}, bool flatMode = false);
 		
 		/**
 		 * Creates a BdfCommentCStyle comment that consists of the specified single line comment.
+		 * @param commentText A single line of text.
+		 * @param indenter The new object's indenter.
+		 * @param flatMode The new object's flatMode.
+		 * @since 1.5.0
+		 * @internal
 		 */
-		BdfCommentCStyle(std::string commentText, Bdf::BdfIndent indenter = {"", ""}, bool flattenSingleLineComments = false);
+		BdfCommentCStyle(std::string commentText, Bdf::BdfIndent indenter = {"", ""}, bool flatMode = false);
 		
 		/**
 		 * Creates a BdfCommentCStyle comment that consists of all the lines given in the vector.
+		 * @param commentLines A vector containing lines of text that will represent the comment.
+		 * @param indenter The new object's indenter.
+		 * @param flatMode The new object's flatMode.
+		 * @since 1.5.0
+		 * @internal
 		 */
-		BdfCommentCStyle(std::vector<std::string> commentLines, Bdf::BdfIndent indenter = {"", ""}, bool flattenSingleLineComments = false);
+		BdfCommentCStyle(std::vector<std::string> commentLines, Bdf::BdfIndent indenter = {"", ""}, bool flatMode = false);
+		
+		/**
+		 * Gets the comment lines of this object.
+		 * @since 1.5.0
+		 */
+		std::vector<std::string> getCommentLines() const;
+		
+		/**
+		 * Gets the indenter of this object.
+		 * @since 1.5.0
+		 */
+		Bdf::BdfIndent getIndenter() const;
+		
+		/**
+		 * Gets whether flat mode is enabled.
+		 * @since 1.5.0
+		 */
+		bool getFlatMode() const noexcept;
+		
+		/**
+		 * Sets the comment lines of this object to the lines given at commentLines.
+		 * @param commentLines The new set of comment lines.
+		 * @since 1.5.0
+		 */
+		void setCommentLines(std::vector<std::string> commentLines);
+		
+		/**
+		 * Sets the indenter of this object to the indenter given at indenter.
+		 * @param indenter The new indenter.
+		 * @since 1.5.0
+		 */
+		void setIndenter(Bdf::BdfIndent indenter);
+		
+		/**
+		 * Sets whether to use flat mode to the bool given at flatMode.
+		 * @param flatMode The new state of flatMode.
+		 * @since 1.5.0
+		 */
+		void setFlatMode(bool flatMode) noexcept;
 		
 		/**
 		 * Deleted (no copy constructor).
--- src/BdfCommentCStyle.cpp
+++ src/BdfCommentCStyle.cpp
@@ -2,20 +2,44 @@
 
 namespace Bdf {
 	// Delegate to vector constructor
-	BdfCommentCStyle::BdfCommentCStyle(std::string commentText, Bdf::BdfIndent indenter, bool flattenSingleLineComments): 
-		BdfCommentCStyle(std::vector<std::string>({commentText}), std::move(indenter), flattenSingleLineComments) {
+	BdfCommentCStyle::BdfCommentCStyle(std::string commentText, Bdf::BdfIndent indenter, bool flatMode): 
+		BdfCommentCStyle(std::vector<std::string>({commentText}), std::move(indenter), flatMode) {
 	}
 	
 	// Stores the given vector
-	BdfCommentCStyle::BdfCommentCStyle(std::vector<std::string> commentLines, Bdf::BdfIndent indenter, bool flattenSingleLineComments): 
+	BdfCommentCStyle::BdfCommentCStyle(std::vector<std::string> commentLines, Bdf::BdfIndent indenter, bool flatMode): 
 		commentLines(std::move(commentLines)),
 		indenter(std::move(indenter)),
-		flattenSingleLineComments(flattenSingleLineComments) {
+		flatMode(flatMode) {
+	}
+		
+	std::vector<std::string> BdfCommentCStyle::getCommentLines() const {
+		return this->commentLines;
+	}
+	
+	Bdf::BdfIndent BdfCommentCStyle::getIndenter() const {
+		return this->indenter;
+	}
+		
+	bool BdfCommentCStyle::getFlatMode() const noexcept {
+		return this-flatMode;
+	}
+	
+	void BdfCommentCStyle::setCommentLines(std::vector<std::string> commentLines) {
+		this->commentLines = std::move(commentLines);
+	}
+	
+	void BdfCommentCStyle::setIndenter(Bdf::BdfIndent indenter) {
+		this->indenter = std::move(indenter);
+	}
+	
+	void BdfCommentCStyle::setFlatMode(bool flatMode) noexcept {
+		this->flatMode = flatMode;
 	}
 	
 	void BdfCommentCStyle::serializeHumanReadable(std::ostream &out, const BdfIndent &indent, int it) const {
 		std::size_t commentLinesSize = this->commentLines.size();
-		if (flattenSingleLineComments && commentLinesSize <= 1) {
+		if (flatMode && commentLinesSize <= 1) {
 			if (commentLinesSize == 0) {
 				
 			}
